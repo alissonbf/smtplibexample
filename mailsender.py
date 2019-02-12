dbc4aa769a9eff1132a2ad3046a3e7a91ca95a48
Mert İnal <mertinal1998@hotmail.com>
2019-02-12 11:11:57 +0300

    Create mailsender.py

---
+++ mailsender.py
@@ -0,0 +1,29 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import sys
+
+mesaj=MIMEMultipart()
+mesaj["From"]="bilgi@mert.space"
+mesaj["To"]="mertinal1998@hotmail.com"
+mesaj["Subject"]="SMTP mail gönderme"
+yazi="""
+SMTP ile mail gönderiyorum 
+Mert Space
+
+-Mert İnal -
+
+"""
+mesajGovdesi=MIMEText(yazi,"plain")
+mesaj.attach(mesajGovdesi)
+try:
+    mail=smtplib.SMTP("mail.mert.space",587)
+    mail.ehlo()
+    mail.starttls()
+    mail.login("bilgi@mert.space","WRITEPASS")
+    mail.sendmail(mesaj["From"],mesaj["To"],mesaj.as_string())
+    print("Mail Başarıyla Gönderildi ..")
+    mail.close()
+except:
+    sys.stderr.write("Bir sorunoluştu ..")
+    sys.stderr.flush()
