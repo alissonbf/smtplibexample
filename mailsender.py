37d58218e9fc6a9348e7fefc5976fe983e238c7a
Alisson Barbosa Ferreira <af.capitani@itmss.com.br>
2020-05-22 11:50:04 -0300

    add utf-8 tag

--- mailsender.py
+++ mailsender.py
@@ -5,27 +5,27 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import sys
 
-mesaj=MIMEMultipart()
-mesaj["From"]="bilgi@mert.space"
-mesaj["To"]="mertinal1998@hotmail.com"
-mesaj["Subject"]="SMTP mail gönderme"
-yazi="""
+mesaj = MIMEMultipart()
+mesaj["From"] = "bilgi@mert.space"
+mesaj["To"] = "mertinal1998@hotmail.com"
+mesaj["Subject"] = "SMTP mail gönderme"
+yazi = """
 SMTP ile mail gönderiyorum 
 Mert Space
 
 -Mert İnal -
 
 """
-mesajGovdesi=MIMEText(yazi,"plain")
+mesajGovdesi = MIMEText(yazi, "plain")
 mesaj.attach(mesajGovdesi)
 try:
-    mail=smtplib.SMTP("mail.mert.space",587)
+    mail = smtplib.SMTP("mail.mert.space", 587)
     mail.ehlo()
     mail.starttls()
-    mail.login("bilgi@mert.space","WRITEPASS")
-    mail.sendmail(mesaj["From"],mesaj["To"],mesaj.as_string())
+    mail.login("bilgi@mert.space", "WRITEPASS")
+    mail.sendmail(mesaj["From"], mesaj["To"], mesaj.as_string())
     print("Mail Başarıyla Gönderildi ..")
     mail.close()
-except:
-    sys.stderr.write("Bir sorunoluştu ..")
+except Exception as error:
+    sys.stderr.write("Bir sorunoluştu .. {0}".format(error))
     sys.stderr.flush()
